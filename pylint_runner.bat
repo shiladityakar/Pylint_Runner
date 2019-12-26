62140c95e804f2ddbc68443d731c1418debbe5c7
shiladityakar <40920082+shiladityakar@users.noreply.github.com>
2019-12-27 02:57:17 +0530

    Creating the pylint_runner.bat file
    Windows script file to run Pylint on Python source code.

---
+++ pylint_runner.bat
@@ -0,0 +1,31 @@
+@echo off
+REM
+REM Version		: 1.0.0
+REM Author		: Shiladitya Kar
+REM
+
+title "pylint_runner.cmd"
+
+set file_name=%1
+
+ECHO Analysing source code
+
+ECHO * Starting the Analysis %date% %time% >> pylint_runner.log
+
+if NOT "%file_name%"=="" (
+	ECHO * Analysing %file_name% >> pylint_runner.log
+	ECHO * Starting Analysis %date% %time% on %file_name%  >> pylint_runner_result.xls
+	pylint %file_name% >> %file_name%.log
+	ECHO * Ending Analysis %date% %time% on %file_name% >> %file_name%.log
+)
+
+if "%file_name%"=="" (
+	for /r %%i in (*.py) do (
+		ECHO * Analysing "%%i" >> pylint_runner.log
+		ECHO * Starting Analysis %date% %time% on "%%i" >> pylint_runner_result.xls
+		pylint "%%i" >> pylint_runner_result.xls
+		ECHO * Ending Analysis %date% %time% on "%%i" >> pylint_runner_result.xls
+	)
+)
+
+ECHO * Ending the Analysis %date% %time% >> pylint_runner.log
